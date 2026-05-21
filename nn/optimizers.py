8202de80941287d70c2d9e40f04e091532160584
Eshaan Dinesh <eshaan.dinesh@outlook.com>
2026-05-22 01:36:20 +0530

    implement SGD optimizer

---
+++ nn/optimizers.py
@@ -0,0 +1,10 @@
+class SGD:
+    def __init__(self, learning_rate=0.01):
+        self.lr = learning_rate
+
+    def step(self, layers):
+        for layer in layers:
+            if hasattr(layer, 'dW'):
+                layer.W = layer.W - self.lr * layer.dW
+                layer.b = layer.b - self.lr * layer.db
+        pass
