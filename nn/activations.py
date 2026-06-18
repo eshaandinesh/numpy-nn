4475830d5f07d1b99f1c09f95c0d1250ce39de56
Eshaan Dinesh <eshaan.dinesh@outlook.com>
2026-06-18 13:43:18 +0530

    implement Sigmoid and Tanh activations

--- nn/activations.py
+++ nn/activations.py
@@ -18,3 +18,42 @@ class ReLU:
         returns dx: (same shape as x)
         """
         return dout * (self._input > 0)
+    
+class Sigmoid:
+    def __init__(self):
+        self._output = None
+
+    def forward(self, x):
+        """
+        x: any shape
+        returns: same shape as x
+        """
+        self._output =  1 / (1 + np.exp(-x))
+        return self._output
+
+    def backward(self, dout):
+        """
+        dout: same shape as x
+        returns: same shape as x
+        """
+        return dout * self._output * (1 - self._output)
+    
+class Tanh:
+    def __init__(self):
+        self._output = None
+
+    def forward(self, x):
+        """
+        x: any shape
+        returns: same shape as x
+        """
+        self._output = np.tanh(x)
+        return self._output
+        
+
+    def backward(self, dout):
+        """
+        dout: any shape
+        returns: same shape as x
+        """
+        return dout * (1 - self._output ** 2)
